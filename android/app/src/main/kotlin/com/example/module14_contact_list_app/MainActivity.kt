22d24e92bfebb1f87c0ac079f728e7886abf3cbc
Nazmul <sakilh@gmail.com>
2024-06-07 23:45:09 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/module14_contact_list_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.module14_contact_list_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,298 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(ContactListApp());
+// }
+//
+// class ContactListApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Contact List App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: ContactListScreen(),
+//     );
+//   }
+// }
+//
+// class ContactListScreen extends StatefulWidget {
+//   @override
+//   _ContactListScreenState createState() => _ContactListScreenState();
+// }
+//
+// class _ContactListScreenState extends State<ContactListScreen> {
+//   final List<Map<String, String>> _contacts = [];
+//   final _nameController = TextEditingController();
+//   final _numberController = TextEditingController();
+//
+//   void _addContact() {
+//     final String name = _nameController.text;
+//     final String number = _numberController.text;
+//
+//     if (name.isNotEmpty && number.isNotEmpty) {
+//       setState(() {
+//         _contacts.add({'name': name, 'number': number});
+//       });
+//
+//       _nameController.clear();
+//       _numberController.clear();
+//     }
+//   }
+//
+//   void _deleteContact(int index) {
+//     setState(() {
+//       _contacts.removeAt(index);
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Contact List'),
+//       ),
+//       body: Column(
+//         children: [
+//           Padding(
+//             padding: const EdgeInsets.all(16.0),
+//             child: Column(
+//               children: [
+//                 TextField(
+//                   controller: _nameController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Name',
+//                   ),
+//                 ),
+//                 TextField(
+//                   controller: _numberController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Number',
+//                   ),
+//                   keyboardType: TextInputType.phone,
+//                 ),
+//                 SizedBox(height: 10),
+//                 ElevatedButton(
+//                   onPressed: _addContact,
+//                   child: Text('Add Contact'),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           Expanded(
+//             child: ListView.builder(
+//               itemCount: _contacts.length,
+//               itemBuilder: (context, index) {
+//                 return GestureDetector(
+//                   onLongPress: () {
+//                     showDialog(
+//                       context: context,
+//                       builder: (BuildContext context) {
+//                         return AlertDialog(
+//                           title: Text('Delete Contact'),
+//                           content: Text('Are you sure you want to delete this contact?'),
+//                           actions: [
+//                             TextButton(
+//                               onPressed: () {
+//                                 Navigator.of(context).pop();
+//                               },
+//                               child: Text('Cancel'),
+//                             ),
+//                             TextButton(
+//                               onPressed: () {
+//                                 _deleteContact(index);
+//                                 Navigator.of(context).pop();
+//                               },
+//                               child: Text('Delete'),
+//                             ),
+//                           ],
+//                         );
+//                       },
+//                     );
+//                   },
+//                   child: ListTile(
+//                     title: Text(_contacts[index]['name']!),
+//                     subtitle: Text(_contacts[index]['number']!),
+//                   ),
+//                 );
+//               },
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ContactListApp());
+}
+
+class ContactListApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contact List App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ContactListScreen(),
+    );
+  }
+}
+
+class ContactListScreen extends StatefulWidget {
+  @override
+  _ContactListScreenState createState() => _ContactListScreenState();
+}
+
+class _ContactListScreenState extends State<ContactListScreen> {
+  final List<Map<String, String>> _contacts = [];
+  final _nameController = TextEditingController();
+  final _numberController = TextEditingController();
+
+  void _addContact() {
+    final String name = _nameController.text;
+    final String number = _numberController.text;
+
+    if (name.isNotEmpty && number.isNotEmpty) {
+      setState(() {
+        _contacts.add({'name': name, 'number': number});
+      });
+
+      _nameController.clear();
+      _numberController.clear();
+    }
+  }
+
+  void _deleteContact(int index) {
+    setState(() {
+      _contacts.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF667c89),
+        title: Center(
+          child: Text(
+            'Contact List',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF667c89), width: 2),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF667c89), width: 2),
+                      ),
+                    ),
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: _numberController,
+                    decoration: InputDecoration(
+                      labelText: 'Number',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF667c89), width: 2),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF667c89), width: 2),
+                      ),
+                    ),
+                    keyboardType: TextInputType.phone,
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _addContact,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF667c89),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      child: Text(
+                        'Add',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _contacts.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onLongPress: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Delete Contact'),
+                            content: Text('Are you sure you want to delete this contact?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Cancel'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  _deleteContact(index);
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Delete'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: ListTile(
+                      title: Text(
+                        _contacts[index]['name']!,
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      subtitle: Text(
+                        _contacts[index]['number']!,
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
